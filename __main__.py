f67a35bcd11e2753c5e5b6a11a264793241681fa
wangzhaohan2910 <wangcf3000@sina.com>
2025-09-16 13:52:04 +0800

    feat: Implement

--- __main__.py
+++ __main__.py
@@ -1,28 +1,55 @@
-# Prototype
-class multimap(defaultdict[str, set[str]]):
-    def __init__(self, *args, **kwargs) -> None
-    def __str__(self) -> str
-    def __repr__(self) -> str
-    def __bool__(self) -> bool
-    def __nonzero__(self) -> bool
-    def __enter__(self) -> 'multimap'
-    def __exit__(self, *args, **kwargs) -> bool
-    def __copy__(self) -> 'multimap'
-    def __deepcopy__(self, memo: dict) -> 'multimap'
-    def __len__(self) -> int
-    def __getitem__(self, key: str) -> set[str]
-    def __setitem__(self, key: str, value: Union[str, set[str]]) -> None
-    def __contains__(self, item: tuple[str]) -> Optional[bool]
-    def remove_key(self, key: str)
-    def remove_value(self, value: str)
-    def remove_item(self, key: str, value: str)
-    def change_key(self, old: str, new: str)
-    def change_value(self, old: str, new: str)
-    def change_item(self, key: str, old: str, new: str)
-    def move_value(self, value: str, old: str, new: str)
-    def keys(self) -> Iterator[str]
-    def values(self) -> Iterator[str]
-    def items(self) -> Iterator[tuple[str]]
-    def asdict(self) -> dict[str, set[str]]
-    def clear(self) -> None
-    def update(self, other: 'multimap') -> None
+from collections import defaultdict
+
+
+class multimap(defaultdict):
+    def __init__(self, **kwargs):
+        super().__init__(set, **kwargs)
+
+    def __str__(self):
+        return "multimap(" + str(dict(self)) + ")"
+
+    def __repr__(self):
+        return str(self)
+
+    def remove_value(self, value):
+        for k in self.keys():
+            self[k].discard(value)
+
+    def remove_item(self, key, value):
+        self[key].remove(value)
+
+    def change_key(self, old, new):
+        self[new].update(self[old])
+        del self[old]
+
+    def change_value(self, old, new):
+        for k in self.keys():
+            if old in self[k]:
+                self[k].remove(old)
+                self[k].add(new)
+
+    def change_item(self, key, old, new):
+        self[key].remove(old)
+        self[key].add(new)
+
+    def move_value(self, value, old, new):
+        self.remove_item(old, value)
+        self[new].add(value)
+
+    def have_item(self, key, value):
+        return value in self[key]
+
+    def update_key(self, key, *values):
+        self[key].update(values)
+
+    def values(self):
+        v = set()
+        for k in self.keys():
+            v.update(self[k])
+        return v
+
+    def items(self):
+        v = []
+        for k in self.keys():
+            v.extend(self[k])
+        return v
