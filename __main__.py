2e4b4bc2ab0834e33330d813d80406546c058fdf
wangzhaohan2910 <wangcf3000@sina.com>
2025-09-23 14:22:23 +0800

    将方法对标C++ multimap。

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ class multimap(defaultdict):
     >>> issubclass(multimap, defaultdict)
     True
     """
+
     def __init__(self):
         """Ctor (Initializer).
 
@@ -32,96 +33,47 @@ class multimap(defaultdict):
 
     __repr__ = __str__
 
-    def remove_item(self, key, value):
-        """Remove a k-v item.
-
-        >>> mp1 = multimap()
-        >>> mp2 = multimap()
-        >>> mp1["foo"] = {"baz", "bar", 0xcafe}
-        >>> mp2["foo"] = {"baz", 0xcafe}
-        >>> mp1[0xfeed] = {998244353, "baz"}
-        >>> mp2[0xfeed] = {998244353, "baz"}
-        
-        >>> mp1.remove_item("foo", "bar")
-        >>> mp1 == mp2
-        True
-        
-        >>> mp2[0xfeed] = {998244353}
-        >>> mp1.remove_item(0xfeed, "baz")
-        >>> mp1 == mp2
-        True
-        
-        >>> del mp2[0xfeed]
-        >>> mp1 == mp2
-        False
-        
-        >>> mp1.remove_item(0xfeed, 998244353)
-        >>> mp1 == mp2
-        True
-        """
-        self[key].remove(value)
-        if self[key] == set():
-            del self[key]
-
-    def change_key(self, old, new):
-        """Rename a key. If the key is already existed, then merge it.
-
-        >>> mp1 = multimap()
-        >>> mp2 = multimap()
-        >>> mp1["foo"] = {"baz", "bar", 0xcafe}
-        >>> mp2["fubar"] = {"baz", "bar", 0xcafe}
-        >>> mp1[0xfeed] = {998244353, "baz"}
-        >>> mp2[0xfeed] = {998244353, "baz"}
-
-        >>> mp1.change_key("foo", "fubar")
-        >>> mp1 == mp2
-        True
-
-        >>> mp1.change_key("fubar", 0xfeed)
-        >>> mp2[0xfeed] = {"baz", "bar", 0xcafe}
-        >>> del mp2["fubar"]
-        >>> mp1 == mp2
-        False
-
-        >>> mp2[0xfeed] = {998244353, "baz"}
-        >>> mp1 == mp2
-        False
-        
-        >>> mp2[0xfeed] = {"baz", "bar", 0xcafe, 998244353}
-        >>> mp1 == mp2
-        True
-        """
-        self[new].update(self[old])
-        del self[old]
-
-    def change_item(self, key, old, new):
-        self[key].remove(old)
-        self[key].add(new)
-
-    def move_value(self, value, old, new):
-        self.remove_item(old, value)
-        self[new].add(value)
-
-    def have_item(self, key, value):
-        return value in self[key]
-
-    def update_key(self, key, *values):
-        self[key].update(values)
+    def keys(self):
+        res = set()
+        for k in super().keys():
+            if self.count(k) == 0:
+                del self[k]
+            else:
+                res.add(k)
+        return res
 
     def values(self):
-        v = set()
+        res = set()
         for k in self.keys():
-            v.update(self[k])
-        return v
+            res.update(self[k])
+        return res
 
     def items(self):
-        l = []
+        res = set()
         for k in self.keys():
             for v in self[k]:
-                l.append((k, v))
-        return l
+                res.add((k, v))
+        return res
 
-    
+    def count(self, key):
+        return len(self[key])
+
+    def __len__(self):
+        res = 0
+        for k in self.keys():
+            res += len(self[k])
+        return res
+
+    def insert(self, key, value):
+        self[key].add(value)
+
+    def erase(self, key, value):
+        self[key].remove(value)
+        if self.count(key) == 0:
+            del self[key]
+
+
 if __name__ == "__main__":
     from doctest import testmod
+
     testmod()
